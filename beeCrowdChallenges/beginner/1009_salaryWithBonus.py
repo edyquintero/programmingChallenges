38dab47e809135a4bce4fc5bf80e83c5bd304a81
edyquintero <edyquintero882@gmail.com>
2023-10-13 23:18:26 -0500

    Solutions for 1009Bc an 100Cf

---
+++ beeCrowdChallenges/beginner/1009_salaryWithBonus.py
@@ -0,0 +1,9 @@
+def calculateTotalSalary(baseSalary, salesAmount):
+    return baseSalary + (salesAmount * 0.15)
+
+
+name = input()
+salary = float(input())
+sales = float(input())
+
+print("TOTAL = R$ {:.2f}".format(calculateTotalSalary(salary, sales)))
---
+++ codeforcesChallenges/withoutCategory/100_A+B.py
@@ -0,0 +1,7 @@
+def add(n1, n2):
+    return int(n1) + int(n2)
+
+
+a, b = (input()).split()
+
+print(add(a, b))
