c097ffbc703201ab5768316d17c930fcedfad53c
edyquintero <edyquintero882@gmail.com>
2025-02-23 18:58:18 -0500

    Saving changes and work

--- beeCrowdChallenges/beginner/1049_Animal.py
+++ beeCrowdChallenges/beginner/1049_Animal.py
@@ -1,1 +1,26 @@
+in1 = input()
+in2 = input()
+in3 = input()
 
+if in1 == "vertebrado":
+    if in2 == "ave":
+        if in3 == "carnivoro":
+            print("aguia")
+        if in3 == "onivoro":
+            print("pomba")
+    if in2 == "mamifero":
+        if in3 == "onivoro":
+            print("homem")
+        if in3 == "herbivoro":
+            print("vaca")
+if in1 == "invertebrado":
+    if in2 == "inseto":
+        if in2 == "hematofago":
+            print("pulga")
+        if in3 == "herbivoro":
+            print("lagarta")
+    if in2 == "anelideo":
+        if in3 == "hematofago":
+            print("sanguessuga")
+        if in3 == "onivoro":
+            print("minhoca")
